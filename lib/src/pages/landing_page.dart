4fd862f3934e7b612270b7df1e19f1f470e28293
Jordan Gonzales <201614047@uns.edu.pe>
2019-10-17 10:17:59 -0500

    Scroll en landing page

--- lib/src/pages/landing_page.dart
+++ lib/src/pages/landing_page.dart
@@ -19,65 +19,66 @@ class _LandingPageState extends State<LandingPage> {
         color: Colors.white,
         child: Swiper(
           itemBuilder: (BuildContext context,int index){
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Image.asset('images/image.png'),
-                Card(
-                  elevation: 10.0,
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    child: Container(
-                      padding: EdgeInsets.all(10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('JANE DOE', textAlign: TextAlign.start,),
-                          Text('25', textAlign: TextAlign.start,),
-                          SizedBox(height: 15.0,),
-                          Text('This is some description about the person, what he/she expects from the partner and also what they want to achieve in the life.', textAlign: TextAlign.justify,),
-                          SizedBox(height: 15.0,),
-                          Text('10 mins away', textAlign: TextAlign.start,),
-                          SizedBox(height: 35.0,),
-                          Row(
-                            children: <Widget>[
-                              MaterialButton(
-                                height: 40.0, 
-                                minWidth: 154, 
-                                color: Colors.red,
-                                textColor: Colors.white, 
-                                child: Row(
-                                  children: <Widget>[
-                                    Icon(Icons.favorite_border),
-                                    Text("Connect",),
-                                  ],
-                                ), 
-                                onPressed: () => {}, 
-                                splashColor: Colors.white,
-                              ),
-                              MaterialButton(
-                                height: 40.0, 
-                                minWidth: 154, 
-                                color: Colors.greenAccent, 
-                                textColor: Colors.white, 
-                                child: Row(
-                                  children: <Widget>[
-                                    Icon(Icons.near_me),
-                                    Text("Message",),
-                                  ],
-                                ), 
-                                onPressed: () => {}, 
-                                splashColor: Colors.white,
-                              ),
-                            ],
-                          ),
-                        ],
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 80.0,),
+                  Image.asset('images/image.png'),
+                  Card(
+                    elevation: 10.0,
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      child: Container(
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('JANE DOE', textAlign: TextAlign.start,),
+                            Text('25', textAlign: TextAlign.start,),
+                            SizedBox(height: 15.0,),
+                            Text('This is some description about the person, what he/she expects from the partner and also what they want to achieve in the life.', textAlign: TextAlign.justify,),
+                            SizedBox(height: 15.0,),
+                            Text('10 mins away', textAlign: TextAlign.start,),
+                            SizedBox(height: 35.0,),
+                            Row(
+                              children: <Widget>[
+                                MaterialButton(
+                                  height: 40.0, 
+                                  minWidth: 154, 
+                                  color: Colors.red,
+                                  textColor: Colors.white, 
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(Icons.favorite_border),
+                                      Text("Connect",),
+                                    ],
+                                  ), 
+                                  onPressed: () => {}, 
+                                  splashColor: Colors.white,
+                                ),
+                                MaterialButton(
+                                  height: 40.0, 
+                                  minWidth: 154, 
+                                  color: Colors.greenAccent, 
+                                  textColor: Colors.white, 
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(Icons.near_me),
+                                      Text("Message",),
+                                    ],
+                                  ), 
+                                  onPressed: () => {}, 
+                                  splashColor: Colors.white,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             );
           },
           itemCount: 10,
